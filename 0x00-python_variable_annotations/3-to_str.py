0c20a4d9aa6ab58ab060bf70abee12a54b3666ba
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-05-03 02:03:40 +0300

    Added task "3. Basic annotations - to string"
    Write a type-annotated function `to_str` that takes a float `n` as argument and returns the string representation of the float.
    
    ```py
    bob@dylan:~$ cat 3-main.py
    #!/usr/bin/env python3
    to_str = __import__('3-to_str').to_str
    
    pi_str = to_str(3.14)
    print(pi_str == str(3.14))
    print(to_str.__annotations__)
    print("to_str(3.14) returns {} which is a {}".format(pi_str, type(pi_str)))
    
    bob@dylan:~$ ./3-main.py
    True
    {'n': <class 'float'>, 'return': <class 'str'>}
    to_str(3.14) returns 3.14, which is a <class 'str'>
    ```
    
    Files created:
    - 3-to_str.py
    - mains/3-main.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Task 3. Basic annotations - to string
+
+Takes a float and returns it's string representation
+"""
+
+
+def to_str(n: float) -> str:
+    """to_str
+
+    Takes a float and returns a string representation
+    of that float
+
+    Arguments:
+        n (float): The inputed float.
+
+    Return:
+        (str): The string representation of the inputed float.
+    """
+    return (str(n))
