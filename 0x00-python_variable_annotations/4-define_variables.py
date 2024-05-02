23140cf7ece7d434c03c1dd385d3bd1a197fbb99
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-05-03 02:15:13 +0300

    Added task "4. Define variables"
    Define and annotate the following variables with the specified values:
    
    - `a`, an integer with a value of 1
    - `pi`, a float with a value of 3.14
    - `i_understand_annotations`, a boolean with a value of True
    - `school`, a string with a value of “Holberton”
    
    ```py
    bob@dylan:~$ cat 4-main.py
    #!/usr/bin/env python3
    
    a = __import__('4-define_variables').a
    pi = __import__('4-define_variables').pi
    i_understand_annotations = __import__('4-define_variables').i_understand_annotations
    school = __import__('4-define_variables').school
    
    print("a is a {} with a value of {}".format(type(a), a))
    print("pi is a {} with a value of {}".format(type(pi), pi))
    print("i_understand_annotations is a {} with a value of {}".format(type(i_understand_annotations), i_understand_annotations))
    print("school is a {} with a value of {}".format(type(school), school))
    
    bob@dylan:~$ ./4-main.py
    a is a <class 'int'> with a value of 1
    pi is a <class 'float'> with a value of 3.14
    i_understand_annotations is a <class 'bool'> with a value of True
    school is a <class 'str'> with a value of Holberton
    ```
    
    Files created:
    - 4-define_variables.py
    - main/4-main.py

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Task 4. Define variables
+
+Defines and annotates variables
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
