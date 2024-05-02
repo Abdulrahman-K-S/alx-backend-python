079e630db74cd25638701945cfc7af55e1696a59
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-05-03 01:41:52 +0300

    Added task "0. Basic annotations - add"
    Write a type-annotated function `add` that takes a float a and `a` float `b` as arguments and returns their sum as a float.
    
    ```py
    bob@dylan:~$ cat 0-main.py
    #!/usr/bin/env python3
    add = __import__('0-add').add
    
    print(add(1.11, 2.22) == 1.11 + 2.22)
    print(add.__annotations__)
    
    bob@dylan:~$ ./0-main.py
    True
    {'a':  <class 'float'>, 'b': <class 'float'>, 'return': <class 'float'>}
    ```
    
    Files created:
    - 0-add.py
    - mains/0-main.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Task 0. Basic annotations - add
+
+Returns sum of floats
+"""
+
+
+def add(a: float, b: float) -> float:
+    """add
+    
+    This function takes 2 floats and adds them then returns it.
+
+    Arguemnts:
+        a (float): The first float to add.
+        b (float): The second float to add.
+    
+    Return:
+        (float): The sum of a & b.
+    """
+    return (a + b)
