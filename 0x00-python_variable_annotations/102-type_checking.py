f7efd76d4f0337d1666c917b3ac646c83d5922d2
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-05-03 02:42:56 +0300

    Added task "12. Type Checking"
    Use `mypy` to validate the following piece of code and apply any necessary changes.
    
    ```py
    def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
        zoomed_in: Tuple = [
            item for item in lst
            for i in range(factor)
        ]
        return zoomed_in
    
    array = [12, 72, 91]
    
    zoom_2x = zoom_array(array)
    
    zoom_3x = zoom_array(array, 3.0)
    ```
    ```py
    bob@dylan:~$ mypy 102-type_checking.py
    Success: no issues found in 1 source file
    bob@dylan:~$ cat 102-main.py
    #!/usr/bin/env python3
    
    zoom_array =  __import__('102-type_checking').zoom_array
    
    print(zoom_array.__annotations__)
    
    bob@dylan:~$ ./102-main.py
    {'lst': typing.Tuple, 'factor': <class 'int'>, 'return': typing.List}
    ```
    
    Files created:
    - 102-type_checking.py

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Task 12. Type Checking
+
+Type Checking
+"""
+
+from typing import List, Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """zoom_array
+
+    returns a list
+    """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = tuple([12, 72, 91])
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, int(3.0))
