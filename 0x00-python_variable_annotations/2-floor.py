00b14d5bdbd67cd6a5458bd5c5bc5dfd7285ad98
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-05-03 01:59:33 +0300

    Added task "2. Basic annotations - floor"
    Write a type-annotated function `floor` which takes a float `n` as argument and returns the floor of the float.
    
    ```py
    bob@dylan:~$ cat 2-main.py
    #!/usr/bin/env python3
    
    import math
    
    floor = __import__('2-floor').floor
    
    ans = floor(3.14)
    
    print(ans == math.floor(3.14))
    print(floor.__annotations__)
    print("floor(3.14) returns {}, which is a {}".format(ans, type(ans)))
    
    bob@dylan:~$ ./2-main.py
    True
    {'n': <class 'float'>, 'return': <class 'int'>}
    floor(3.14) returns 3, which is a <class 'int'>
    ```
    
    Files created:
    - 2-floor.py
    - mains/2-main.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Task 2. Basic annotations - floor
+
+Takes a float and returns it's floor
+"""
+
+
+def floor(n: float) -> int:
+    """floor
+
+    Takes a float variable and returns it's floor
+
+    Arguements:
+        n (float): The float to get its floor.
+
+    Return:
+        (int): The floor of the inputed variable.
+    """
+    return (int(n))
