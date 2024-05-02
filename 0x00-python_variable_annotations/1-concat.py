ee4d569b0b8630bb236b0f8d72107b8db258774e
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-05-03 01:55:31 +0300

    Added task "1. Basic annotations - concat"
    Write a type-annotated function `concat` that takes a string `str1` and a string `str2` as arguments and returns a concatenated string
    
    ```py
    bob@dylan:~$ cat 1-main.py
    #!/usr/bin/env python3
    concat = __import__('1-concat').concat
    
    str1 = "egg"
    str2 = "shell"
    
    print(concat(str1, str2) == "{}{}".format(str1, str2))
    print(concat.__annotations__)
    
    bob@dylan:~$ ./1-main.py
    True
    {'str1': <class 'str'>, 'str2': <class 'str'>, 'return': <class 'str'>}
    ```
    
    Files created:
    - 1-concat.py
    - mains/1-main.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Task 1. Basic annotations - concat
+
+Return the concatenation of 2 strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concat
+
+    Takes 2 strings as input and returns its concatenation.
+
+    Arguments:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Return:
+        (str): str1 + str2
+    """
+    return (str1 + str2)
