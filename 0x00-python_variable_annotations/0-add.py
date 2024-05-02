4553d58d6707a1271d42b2929e91af79f777698b
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-05-03 01:49:24 +0300

    Update 0-add.py
    Fixed the indentation for pycodestyle

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -8,13 +8,13 @@ Returns sum of floats
 
 def add(a: float, b: float) -> float:
     """add
-    
+
     This function takes 2 floats and adds them then returns it.
 
     Arguemnts:
         a (float): The first float to add.
         b (float): The second float to add.
-    
+
     Return:
         (float): The sum of a & b.
     """
