eb703ccad50f698a601ae90140c69dc11af050e2
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-05-07 22:38:26 +0300

    Added task "0. Async Generator"
    Write a coroutine called `async_generator` that takes no arguments.
    
    The coroutine will loop 10 times, each time asynchronously wait 1 second, then yield a random number between 0 and 10. Use the `random` module.
    
    '''py
    bob@dylan:~$ cat 0-main.py
    #!/usr/bin/env python3
    
    import asyncio
    
    async_generator = __import__('0-async_generator').async_generator
    
    async def print_yielded_values():
        result = []
        async for i in async_generator():
            result.append(i)
        print(result)
    
    asyncio.run(print_yielded_values())
    
    bob@dylan:~$ ./0-main.py
    [4.403136952967102, 6.9092712604587465, 6.293445466782645, 4.549663490048418, 4.1326571686139015, 9.99058525304903, 6.726734105473811, 9.84331704602206, 1.0067279479988345, 1.3783306401737838]
    ```
    
    Files:
    - 0-async_generator.py

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Task 0. Async Generator
+
+async_generator coroutine.
+"""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """async_generator
+
+    A coroutine that loops 10 times and each time it waits 1 second
+    and then yeild a number from 1 to 10.
+
+    Return:
+        (AsyncGenerator[float, None]): A float generated from an async
+        coroutine.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
