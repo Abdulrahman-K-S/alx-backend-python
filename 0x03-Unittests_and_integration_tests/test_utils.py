583ee5c9c635c4b2ef5670fde1eb4023c99aae37
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-05-27 16:04:15 +0300

    Add task "0. Parameterize a unit test"
    Familiarize yourself with the `utils.access_nested_map` function and understand its purpose. Play with it in the Python console to make sure you understand.
    
    In this task you will write the first unit test for `utils.access_nested_map`.
    
    Create a `TestAccessNestedMap` class that inherits from `unittest.TestCase`.
    
    Implement the `TestAccessNestedMap.test_access_nested_map` method to test that the method returns what it is supposed to.
    
    Decorate the method with `@parameterized.expand` to test the function for following inputs:
    ```
    nested_map={"a": 1}, path=("a",)
    nested_map={"a": {"b": 2}}, path=("a",)
    nested_map={"a": {"b": 2}}, path=("a", "b")
    ```
    
    For each of these inputs, test with assertEqual that the function returns the expected result.
    
    The body of the test method should not be longer than 2 lines.
    
    Files:
    - test_utils.py

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Test cases for utils.py
+"""
+
+from utils import access_nested_map
+from parameterized import parameterized
+import unittest
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """TestAccessNestedMap
+
+    This class is for testing the nested map functiom.
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, map, path, expected_output):
+        """ Test method returns correct output """
+        real_output = access_nested_map(map, path)
+        self.assertEqual(real_output, expected_output)
+
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, map, path, wrong_output):
+        """ Test method raises correct exception """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(map, path)
+            self.assertEqual(wrong_output, e.exception)
